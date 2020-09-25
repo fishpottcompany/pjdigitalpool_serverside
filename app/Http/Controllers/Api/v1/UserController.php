560db26ce43c987268c46f90af04e9566bae3740
zatana <annodankyikwaku@gmail.com>
2020-09-25 16:29:44 +0000

    Updated success response type to int values

--- app/Http/Controllers/Api/v1/UserController.php
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -1,5 +1,5 @@
 <?php
-
+//%sE7RbhU+,g$[.BT
 namespace App\Http\Controllers\Api\v1;
 
 use App\Models\v1\User;
@@ -42,7 +42,7 @@ class UserController extends Controller
         $accessToken = $user->createToken("authToken", [$validatedData["user_scope"]])->accessToken;
 
         return response([
-            "status" => "success",
+            "status" => 1,
             "user" => $user, 
             "access_token" => $accessToken
             ]);
@@ -68,12 +68,12 @@ class UserController extends Controller
 
         if (!auth()->attempt($login_data)) {
             $log_controller->save_log("member", $request->user_phone_number, "Login|User", "Login failed");
-            return response(["status" => "fail", "message" => "Invalid Credentials"]);
+            return response(["status" => 0, "message" => "Invalid Credentials"]);
         }
 
         if (auth()->user()->user_flagged) {
             $log_controller->save_log("member", $request->user_phone_number, "Login|User", "Login failed because user is flagged");
-            return response(["status" => "fail", "message" => "Account access restricted"]);
+            return response(["status" => 0, "message" => "Account access restricted"]);
         }
 
         $accessToken = auth()->user()->createToken("authToken", [auth()->user()->user_scope])->accessToken;
@@ -81,7 +81,7 @@ class UserController extends Controller
         $log_controller->save_log("member", $request->user_phone_number, "Login|User", "Login successful");
 
         return response([
-            "status" => "success",
+            "status" => 1,
             "user_firstname" => auth()->user()->user_firstname,
             "user_surname" => auth()->user()->user_surname,
             "access_token" => $accessToken
@@ -99,10 +99,10 @@ class UserController extends Controller
     public function logout(Request $request)
     {
         if (!Auth::guard('api')->check()) {
-            return response(["status" => "fail", "message" => "Permission Denied. Please log out and login again"]);
+            return response(["status" => 0, "message" => "Permission Denied. Please log out and login again"]);
         }
         $request->user()->token()->revoke();
-        return response(["status" => "success", "message" => "Logged out successfully"]);
+        return response(["status" => 1, "message" => "Logged out successfully"]);
     }
 
     /*
@@ -128,12 +128,12 @@ class UserController extends Controller
         
         if(!isset($user->user_id)) {
             $log_controller->save_log("member", $request->user_phone_number, "ResetPassword|User", "Operation failed because user was not found");
-            return response(["status" => "fail", "message" => "Account not found"]);
+            return response(["status" => 0, "message" => "Account not found"]);
         }
 
         if($user->user_flagged) {
             $log_controller->save_log("member", $request->user_phone_number, "ResetPassword|User", "Operation failed because user is flagged");
-            return response(["status" => "fail", "message" => "Account access restricted"]);
+            return response(["status" => 0, "message" => "Account access restricted"]);
         }
 
         $resetcode = $resetcode_controller->generate_resetcode();
@@ -150,7 +150,7 @@ class UserController extends Controller
         $log_controller->save_log("member", $request->user_phone_number, "ResetPassword|User", "Reset code sent for password reset");
 
         return response([
-            "status" => "success", 
+            "status" => 1, 
             "message" => "Reset code has been sent to your email"
         ]);
     }
@@ -180,12 +180,12 @@ public function verify_reset_code(Request $request)
 
     if(!isset($user->user_id)) {
         $log_controller->save_log("member", $request->user_phone_number, "ResetPassword|User", "Operation failed because user was not found");
-        return response(["status" => "fail", "message" => "Account not found"]);
+        return response(["status" => 0, "message" => "Account not found"]);
     }
 
     if($user->user_flagged) {
         $log_controller->save_log("member", $request->user_phone_number, "ResetPassword|User", "Operation failed because user is flagged");
-        return response(["status" => "fail", "message" => "Account access restricted"]);
+        return response(["status" => 0, "message" => "Account access restricted"]);
     }
 
     $resetcode = Resetcode::where([
@@ -205,9 +205,9 @@ public function verify_reset_code(Request $request)
         $user->save();
 
         $resetcode_controller->update_resetcode($resetcode[0]["resetcode_id"], $resetcode[0]["user_type"], $resetcode[0]["user_id"], $resetcode[0]["resetcode"], true);
-        return response(["status" => "success", "message" => "Password reset successful"]);
+        return response(["status" => 1, "message" => "Password reset successful"]);
     } else {
-        return response(["status" => "fail", "message" => "Reset failed"]);
+        return response(["status" => 0, "message" => "Reset failed"]);
     }
 }
 
