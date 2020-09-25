35bb139f481f8b34a6fd73bc06211ae37f633e63
zatana <annodankyikwaku@gmail.com>
2020-09-25 02:14:55 +0000

    Removed default user api route

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 
 Route::post('/v1/member/register', 'Api\v1\UserController@register');
