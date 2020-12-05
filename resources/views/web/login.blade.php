03c8cbd33dd280edb8ca1f811af35999465036f6
zatana <annodankyikwaku@gmail.com>
2020-12-05 20:52:33 +0000

    update

--- resources/views/web/login.blade.php
+++ resources/views/web/login.blade.php
@@ -11,7 +11,7 @@
     <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
     <!-- Title -->
-    <title>Admin: PJ Digital Pool</title>
+    <title>Admin: The Glory Hub</title>
 
     <!-- Favicon -->
     <link rel="icon" href="/img/core-img/favicon.png">
